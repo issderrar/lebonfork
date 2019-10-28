a443af2a78a384563ce63693954d1aeaafb60f85
Issam Derrar <issam_derrar@yahoo.fr>
2019-10-28 11:04:02 +0100

    Navbar working with 2 pages

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,31 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
-class HomeController
+class HomeController extends AbstractController
 {
-    public function __construct($twig)
+    /**
+     * @var Environment
+     */
+
+    private $twig;
+
+    public function __construct(Environment $twig)
     {
         $this->twig = $twig;
     }
 
+    /**
+     * @return Response
+     * @Route("/", name="home")
+     */
+
     public function index(): Response
     {
-        return new Response($this->twig->render('pages/home.html.twig'));
+        return $this->render('pages/home.html.twig');
     }
 }
